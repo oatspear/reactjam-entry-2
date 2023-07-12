9e228bda7885ad87dc92bfee88860775d58e5adf
Santusk Oatspear <103125850+oatspear@users.noreply.github.com>
2023-07-12 17:04:02 +0100

    Create MeterBar.tsx

---
+++ src/components/MeterBar.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './MeterBar.css';
+
+interface MeterBarProps {
+  steps: number;
+  initialValue: number;
+}
+
+const MeterBar: React.FC<MeterBarProps> = ({ steps, initialValue }) => {
+  const [value, setValue] = useState(initialValue);
+  const [markerPosition, setMarkerPosition] = useState(0);
+
+  useEffect(() => {
+    const newPosition = (value / (steps - 1)) * 100;
+    setMarkerPosition(newPosition);
+  }, [value, steps]);
+
+  const handleClick = (step: number) => {
+    setValue(step);
+  };
+
+  return (
+    <div className="meter-bar">
+      <div className="marker" style={{ left: `${markerPosition}%` }}>
+        {value}
+      </div>
+      <div className="bar">
+        {Array.from({ length: steps }, (_, index) => (
+          <div
+            key={index}
+            className={`step ${index === value ? 'active' : ''}`}
+            onClick={() => handleClick(index)}
+          >
+            {index}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MeterBar;
+
+/*
+import React from 'react';
+import MeterBar from './MeterBar';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <MeterBar steps={7} initialValue={3} />
+    </div>
+  );
+};
+
+export default App;
+
+*/
