8b0870ac9c3c74b207924eac6bb856f2f1f17061
oatspear <oatspear@gmail.com>
2023-10-05 08:42:58 +0100

    update logic

--- src/logic.ts
+++ src/logic.ts
@@ -1,4 +1,24 @@
+// -----------------------------------------------------------------------------
+// Imports
+// -----------------------------------------------------------------------------
+
 import type { Players, RuneClient } from "rune-games-sdk/multiplayer"
+
+// -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
+
+
+export const TIME_PER_TURN: number = 45;  // seconds
+export const MINIONS_PER_LOCATION: number = 4;
+
+
+export enum Role {
+  TANK = 1,
+  DAMAGE,
+  SUPPORT,
+}
+
 
 // -----------------------------------------------------------------------------
 // Player State
@@ -19,18 +39,45 @@ function newPlayerState(id: string, team: number, deck?: Array<number>): PlayerS
 
 
 // -----------------------------------------------------------------------------
+// Minion State
+// -----------------------------------------------------------------------------
+
+
+export interface MinionState {
+  index: number;
+  minionType: number;
+  power: number;
+  ability: number;
+  role: Role;
+  powerBonus: number;
+}
+
+
+function newMinionState(index: number, minionType: number): MinionState {
+  return {
+    index,
+    minionType,
+    power: 1,
+    ability: 0,
+    role: Role.TANK,
+    powerBonus: 0,
+  };
+}
+
+
+// -----------------------------------------------------------------------------
 // Location State (One Component of the Battle Board)
 // -----------------------------------------------------------------------------
 
 
 export interface LocationState {
   index: number;
-  minions: Array<Array<number>>;
+  minions: Array<Array<MinionState | null>>;
 }
 
 
-function newMinionSlotsArray(): Array<number> {
-  return [0, 0, 0, 0];
+function newMinionSlotsArray(): Array<MinionState | null> {
+  return [null, null, null, null];
 }
 
 
@@ -45,15 +92,66 @@ function newLocationState(index: number): LocationState {
 
 
 export interface GameState {
-  count: number,
+  turnsTaken: number,
+  timer: number,
   locations: Array<LocationState>,
   players: Array<PlayerState>
 }
 
 
-export function getCount(game: GameState) {
-  return game.count;
+function resolveCombatPhase(game: GameState): void {
+  
 }
+
+
+function resolveLocationCombat(location: LocationState): void {
+
+}
+
+
+function sortInAttackingOrder(minions: MinionState[]): MinionState[][] {
+  // order is DAMAGE > TANK > SUPPORT
+  const sorted: MinionState[][] = [[], [], []];
+  for (const minion of minions) {
+    switch (minion.damageType) {
+      case DamageType.RANGED:
+        sorted[0].push(minion);
+        break;
+      case DamageType.MELEE:
+        sorted[1].push(minion);
+        break;
+      case DamageType.SPELL:
+        sorted[2].push(minion);
+        break;
+    }
+  }
+  return sorted;
+}
+
+
+function sortInDefendingOrder(minions: MinionState[]): MinionState[] {
+  // order is MELEE > SPELL > RANGED
+  const sorted = [];
+  const spell = [];
+  const ranged = [];
+  for (const minion of minions) {
+    switch (minion.damageType) {
+      case DamageType.MELEE:
+        sorted.push(minion);
+        break;
+      case DamageType.SPELL:
+        spell.push(minion);
+        break;
+      case DamageType.RANGED:
+        ranged.push(minion);
+        break;
+    }
+  }
+  sorted.push.apply(sorted, spell);
+  sorted.push.apply(sorted, ranged);
+  return sorted;
+}
+
 
 // -----------------------------------------------------------------------------
 // Game Actions
@@ -61,8 +159,7 @@ export function getCount(game: GameState) {
 
 
 type GameActions = {
-  increment: (params: { amount: number }) => void
-  spawnMinion: (params: { location: number, team: number, index: number }) => void
+  spawnMinion: (params: { location: number, team: number, minionType: number }) => void
   clearMinions: () => void
 };
 
@@ -83,7 +180,8 @@ Rune.initLogic({
 
   setup(): GameState {
     return {
-      count: 0,
+      turnsTaken: 0,
+      timer: TIME_PER_TURN,
       locations: [
         newLocationState(0),
         newLocationState(1),
@@ -96,22 +194,27 @@ Rune.initLogic({
     }
   },
 
-  actions: {
-    increment({ amount }, { game }) {
-      game.count += amount
-    },
+  update: ({ game }) => {
+    if (game.timer <= 0) {
+      resolveCombatPhase(game);
+      game.timer = TIME_PER_TURN;
+    } else {
+      --game.timer;
+    }
+  },
 
-    spawnMinion({ location, team, index }, { game }) {
-      game.locations[location].minions[team][index] = 1;
+  actions: {
+    spawnMinion({ location, team, minionType }, { game }) {
+      const minions: MinionState[] = game.locations[location].minions[team];
+      const index: number = minions.length
+      if (minions.length < MINIONS_PER_LOCATION) {
+        minions.push(newMinionState(index, minionType));
+      }
     },
 
     clearMinions(_noargs, { game }) {
       for (const location of game.locations) {
-        for (const team of location.minions) {
-          for (let i = 0; i < team.length; ++i) {
-            team[i] = 0;
-          }
-        }
+        location.minions = [];
       }
     },
   },
