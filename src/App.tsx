db001971eab3c836287dec6afdac286c312adf72
oatspear <oatspear@gmail.com>
2023-10-16 19:04:59 +0100

    WIP UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import { useEffect, useState } from "react"
-import reactLogo from "./assets/rune.svg"
-import viteLogo from "/vite.svg"
 
 import "./App.css"
-import { EventType, GameState, PlayerIndex, getPlayerIndex } from "./logic.ts"
+import { EventType, GameState, PlayerIndex, PlayerState, getPlayerIndex } from "./logic.ts"
 
 import ModalPopup from './components/ModalPopup';
-// import MeterBar from './components/MeterBar';
 import BattlefieldView, { BattlefieldCallbacks } from './components/BattlefieldView.tsx';
 import ActionBar from './components/ActionBar';
 
 
+type PlayersObject = Record<string, { playerId: string, displayName: string, avatarUrl: string }>;
+
+interface ClientPlayerInfo {
+  playerId: string;
+  displayName: string;
+  avatarUrl: string;
+  index: PlayerIndex;
+  resources: number;
+}
+
+interface DisplayMinionStats {
+  power: number;
+  health: number;
+  movement: number;
+}
+
 enum UIState {
   INITIAL = 0,
+  ANIMATING,
   INPUT_MAIN,
   INPUT_MOVE,
+  INPUT_ATTACK,
+  INPUT_PLAYER_BENCH,
+  INPUT_PLAYER_TECH,
+  INPUT_PLAYER_GRAVEYARD,
+  INPUT_ENEMY_MINION,
+  INPUT_ENEMY_BENCH,
+  INPUT_ENEMY_TECH,
+  INPUT_ENEMY_GRAVEYARD,
 }
 
 
@@ -24,14 +46,59 @@ function getWindowDimensions() {
 }
 
 
-function App() {
-  const [uiState, setUiState] = useState<UIState>(UIState.INITIAL);
-  const [game, setGame] = useState<GameState>();
-  const [myPlayerId, setMyPlayerId] = useState<string | undefined>();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isActionBarVisible, setIsActionBarVisible] = useState(false);
+function getTopBottomPlayers(
+  game: GameState,
+  playerIndex: PlayerIndex,
+  players: PlayersObject
+): ClientPlayerInfo[] {
+  const clientPlayers = [];
+  for (const state of game.players) {
+    clientPlayers.push({ ...players[state.id], index: state.index, resources: state.resources });
+  }
+  if (playerIndex === PlayerIndex.PLAYER1) {
+    return clientPlayers.reverse();
+  }
+  return clientPlayers;
+}
 
-  const [selectedTile, setSelectedTile] = useState<number | undefined>();
+
+function newEnemyActionBar(game: GameState, enemyIndex: PlayerIndex): JSX.Element {
+  const player: PlayerState = game.players[enemyIndex];
+  return (
+    <div className="enemy-action-bar">
+      <div className="bench">B</div>
+      <div className="tech">T</div>
+      <div className="graveyard">G</div>
+    </div>
+  );
+}
+
+
+function newPlayerActionBar(game: GameState, playerIndex: PlayerIndex, uiState: UIState): JSX.Element {
+  const player: PlayerState = game.players[playerIndex];
+  return (
+    <div className="player-action-bar">
+      <div className="bench">B</div>
+      <div className="tech">T</div>
+      <div className="graveyard">G</div>
+    </div>
+  );
+}
+
+
+function App() {
+  // game state
+  const [game, setGame] = useState<GameState | undefined>();
+  const [myPlayerId, setMyPlayerId] = useState<string | undefined>();
+  const [players, setPlayers] = useState<PlayersObject>({});
+  // layer visibility
+  const [uiState, setUiState] = useState<UIState>(UIState.INITIAL);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isActionBarVisible, setIsActionBarVisible] = useState<boolean>(false);
+  // UI state variables
+  const [selectedTile, setSelectedTile] = useState<number>(-1);
+  const [actionableTiles, setActionableTiles] = useState<number[]>([]);
+  const [displayStats, setDisplayStats] = useState<DisplayMinionStats | undefined>();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -62,9 +129,17 @@ function App() {
 
   useEffect(() => {
     Rune.initClient({
-      onChange: ({ newGame, yourPlayerId }) => {
+      onChange: ({ newGame, yourPlayerId, players }) => {
         setGame(newGame);
         setMyPlayerId(yourPlayerId);
+        setPlayers({
+          ...players,
+          AI: {
+            playerId: "AI",
+            displayName: "AI (Bot)",
+            avatarUrl: "",
+          },
+        });
       },
     })
   }, []);  
@@ -74,57 +149,63 @@ function App() {
   }
 
   const { height, width } = getWindowDimensions();
+
   const playerIndex: PlayerIndex = getPlayerIndex(game, myPlayerId);
+  const [enemy, player] = getTopBottomPlayers(game, playerIndex, players);
 
   const battlefieldCallbacks: BattlefieldCallbacks = {
     onTileSelected(i: number) {
+      if (uiState === UIState.ANIMATING) { return }
+      setSelectedTile(i);
       const tile = game.battlefield.tiles[i];
-      const minion = game.battlefield.minions[tile.minion]
+      const minion = game.battlefield.minions[tile.minion];
       if (minion != null) {
+        setDisplayStats({ power: minion.power, health: minion.health, movement: minion.movement });
         if (minion.owner === playerIndex) {
           alert("Selected your own minion")
           const from = i;
           const to = i + 1;
           Rune.actions.move({ from, to });
+          setActionableTiles([]);
         } else {
           alert("Selected an enemy minion")
         }
+      } else {
+        setDisplayStats(undefined);
       }
     }
   };
 
+  const showPlayerActionBar: boolean = uiState != UIState.ANIMATING;
+  const showEnemyActionBar: boolean = uiState != UIState.ANIMATING;
+
   return (
     <>
+      <div className="enemy-action-bar">
+        { showEnemyActionBar && newEnemyActionBar(game, enemy.index) }
+      </div>
+
       <div className="flex-column-centered">
         <code>width: {width} ~ height: {height}</code>
+        <p><b>{enemy.displayName}</b></p>
         <BattlefieldView battlefield={game.battlefield} player={playerIndex} callbacks={battlefieldCallbacks} />
+        <p><b>{player.displayName}</b></p>
+
+        <div className="hud">
+          { !!displayStats && <div className="minion-stats">Stats</div> }
+          <div className="resources"></div>
+        </div>
+
+        <div className="main-action-panel">
+          { showPlayerActionBar && newPlayerActionBar(game, playerIndex, uiState) }
+        </div>
+
         <button onClick={toggleActionBar}>Toggle Action Bar</button>
         <ActionBar isVisible={isActionBarVisible} actions={barActions} />
+
         <button onClick={openModal}>Open Modal</button>
-        {isModalOpen && (
-          <ModalPopup message="Hello, I'm a modal!" onClose={closeModal} />
-        )}
+        { isModalOpen && <ModalPopup message="Hello, I'm a modal!" onClose={closeModal} /> }
       </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://developers.rune.ai" target="_blank">
-          <img src={reactLogo} className="logo rune" alt="Rune logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button>
-          count is {game.turnsTaken}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> or <code>src/logic.ts</code> and save to
-          test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Rune logos to learn more
-      </p>
     </>
   )
 }
